341558d7911309e24a59b446bc9c1c2c1b5d51d5
afiliado9591-create <afiliado9591@gmail.com>
2026-09-07 13:23:12 -0300

    Atualizar Postador Mobile para versão 0.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.postadormobile.app"
         minSdk = 23
         targetSdk = 36
-        versionCode = 4
-        versionName = "0.4"
+        versionCode = 5
+        versionName = "0.5"
     }
 
     buildFeatures {

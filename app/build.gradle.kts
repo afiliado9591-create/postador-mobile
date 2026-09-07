af31c65a162cc47b59a0ae45c4a5324cf8b3c2c8
afiliado9591-create <afiliado9591@gmail.com>
2026-09-07 07:30:27 -0300

    Atualizar versão para 0.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.postadormobile.app"
         minSdk = 23
         targetSdk = 36
-        versionCode = 3
-        versionName = "0.3"
+        versionCode = 4
+        versionName = "0.4"
     }
 
     buildFeatures {
